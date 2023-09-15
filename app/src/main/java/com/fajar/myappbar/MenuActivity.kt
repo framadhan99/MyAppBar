7334e3611aa70f5e3caa70d378e5965a73a19f36
framadhan99 <fajarramadhan99704@gmail.com>
2023-09-15 08:00:28 +0700

    search vie

--- app/src/main/java/com/fajar/myappbar/MenuActivity.kt
+++ app/src/main/java/com/fajar/myappbar/MenuActivity.kt
@@ -2,10 +2,27 @@ package com.fajar.myappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.fajar.myappbar.databinding.ActivityMainBinding
+import com.fajar.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView.editText.setOnEditorActionListener { textView, actionId, event ->
+                searchBar.text = searchView.text
+                searchView.hide()
+                Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
     }
 }
